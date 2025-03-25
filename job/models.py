23b1507ee29cd4a87fe3c5c5c050e12244ec733e
AbdelrahmanMustafaHajjaj <abdelrahmanmostafahajjaj@gmail.com>
2025-03-25 18:10:30 +0200

    adding template of joblist and jb details

--- job/models.py
+++ job/models.py
@@ -8,11 +8,16 @@ JOB_TYPE = (
 class job(models.Model):
     title = models.CharField(max_length=100)
     #location 
-    job_type = models.CharField(max_length= 15 , choices = JOB_TYPE, default = '')
+    job_type = models.CharField(max_length= 15 , choices = JOB_TYPE)
     description = models.TextField(max_length= 10000, default = '')
     publishes_at = models.DateTimeField(auto_now=True)
     vacancy = models.IntegerField(default= 1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=0)
+    category = models.ForeignKey('category',on_delete=models.CASCADE)
     def __str__(self):
         return self.title 
+class category(models.Model):
+    name = models.CharField(max_length = 100)
+    def __str__(self):
+        return self.name 
--- outline.md
+++ outline.md
@@ -1,1 +1,29 @@
-.  
+job 
+ - title 
+ - location 
+ - job type (full time , part time)  
+ -description 
+ - published at 
+ - available vaccancies 
+ - salary 
+ - categoury 
+ - experience 
+ - user gender 
+---> Feature 
+    - apply for job 
+    - post job 
+blog: 
+    - title 
+    - descreption 
+    - created_at 
+    - category 
+    - athor 
+    - tags 
+    - comments 
+--------> Feature 
+    - search 
+    - comments 
+    - recent posts
+contact:
+home :
+
